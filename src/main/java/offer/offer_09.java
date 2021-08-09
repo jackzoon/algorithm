34caffd9618388c86c6237249af38015148c7aac
Halen <911452497@qq.com>
2021-08-09 20:21:41 +0800

    offer

---
+++ src/main/java/offer/offer_09.java
@@ -0,0 +1,40 @@
+package offer;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+ */
+
+class CQueue {
+
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
+    public CQueue() {
+
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if (stack1.isEmpty() && stack2.isEmpty()) {
+            return -1;
+        }
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
+
+public class offer_09 {
+
+    public static void main(String[] args) {
+
+    }
+}
